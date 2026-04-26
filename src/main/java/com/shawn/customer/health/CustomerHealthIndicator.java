316812075867a364a723270012ec43d5c5c366de
shawn <zxf9720@gmail.com>
2026-04-26 12:59:57 -0400

    upgrade to Spring Boot 4.0.6 1) update HealthIndicator 2) upgrade spring doc version

--- src/main/java/com/shawn/customer/health/CustomerHealthIndicator.java
+++ src/main/java/com/shawn/customer/health/CustomerHealthIndicator.java
@@ -3,14 +3,13 @@ package com.shawn.customer.health;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
