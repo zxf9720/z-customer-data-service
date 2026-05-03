5d36c660516d91fe74530ba03175b76198c70092
shawn <zxf9720@gmail.com>
2026-05-03 18:24:37 -0400

    code clean up

--- src/main/java/com/shawn/customer/filter/RequestLoggingFilter.java
+++ src/main/java/com/shawn/customer/filter/RequestLoggingFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
